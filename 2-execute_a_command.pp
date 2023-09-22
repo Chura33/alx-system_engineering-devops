b473314a5809a2bf721e34ec38eda1bbadd06d61
Chura33 <churabrady@gmail.com>
2023-09-22 14:22:14 -0700

    solved task 2

--- 2-execute_a_command.pp
+++ 2-execute_a_command.pp
@@ -1,10 +1,8 @@
 # Puppet Manifest to Kill a Process
 
 exec { 'kill_killmenow_process':
-  command   => 'pkill -f killmenow',
-  onlyif    => 'pgrep -f killmenow',
+  command   => 'pkill -f killmenow || [ $? -eq 1 ]',
   provider  => shell,
   timeout   => 60,
   logoutput => true,
 }
-
