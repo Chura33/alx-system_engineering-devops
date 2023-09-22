66ecb0c564e5e7f5f27c9fe5cbc4f6b96e1e8274
Chura33 <churabrady@gmail.com>
2023-09-22 14:08:37 -0700

    solved task 2

---
+++ 2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Puppet Manifest to Kill a Process
+
+exec { 'kill_killmenow_process':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  onlyif      => 'pgrep -f killmenow',
+  refreshonly => true,
+}
