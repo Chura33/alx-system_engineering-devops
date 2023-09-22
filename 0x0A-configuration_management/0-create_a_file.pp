566f6f66320b5a956c15635a8798f3cf43447439
Chura33 <churabrady@gmail.com>
2023-09-22 14:02:42 -0700

    removed empty line

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,14 +1,15 @@
 # This file creates a newfile
 # school and gives it permissions
 # and also writes content inside it
+require 'uri'
+
+$my_text = 'I love Puppet'
+$encoded_text = URI.encode_www_form_component($my_text)
 
 file { '/tmp/school':
   ensure  => 'file',
-  content => 'I love Puppet',
-}
-exec { 'set_school_permissions':
-  command => 'chmod 0744 /tmp/school && chown www-data:www-data /tmp/school',
-  path    => '/bin:/usr/bin',
-  creates => '/tmp/school',
-  require => File['/tmp/school'],
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => $encoded_text,
 }
