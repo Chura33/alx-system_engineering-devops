05f95ec14a3d51118d5e23960406d9fd887759cc
Chura33 <churabrady@gmail.com>
2023-09-22 13:52:23 -0700

    solved task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,14 @@
-exec { 'create_school_file':
-  command => 'echo "I love Puppet" > /tmp/school && chmod 0744 /tmp/school && chown www-data:www-data /tmp/school',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  creates => '/tmp/school',
-}
+# This file creates a newfile
+# school and gives it permissions
+# and also writes content inside it
 
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+}
+exec { 'set_school_permissions':
+  command => 'chmod 0744 /tmp/school && chown www-data:www-data /tmp/school',
+  path    => '/bin:/usr/bin',
+  creates => '/tmp/school',
+  require => File['/tmp/school'],
+}
