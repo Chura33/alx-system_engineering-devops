93b9026b9c068edeff51fe2e540815db5645517a
Chura33 <churabrady@gmail.com>
2023-09-22 14:05:15 -0700

    solved task 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,15 +1,9 @@
-# This file creates a newfile
-# school and gives it permissions
-# and also writes content inside it
-require 'uri'
-
-$my_text = 'I love Puppet'
-$encoded_text = URI.encode_www_form_component($my_text)
+# Puppet Manifest to Create a File
 
 file { '/tmp/school':
   ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => $encoded_text,
+  content => 'I love Puppet',
 }
