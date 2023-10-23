0df87bac9be3f12cb5a49660d9036efe353c89ef
Chura33 <churabrady@gmail.com>
2023-10-23 13:37:52 -0700

    solved task 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This script gets information of all users
+and puts them in a json file
+"""
+
+
+import requests
+from sys import argv
+import json
+
+
+users = requests.get("https://jsonplaceholder.typicode.com/users")
+users = users.json()
+
+todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+todos = todos.json()
+todoAll = {}
+
+for user in users:
+    taskList = []
+    for todo in todos:
+        if todo.get('userId') == user.get('id'):
+            taskList.append({
+                "username": user.get('username'),
+                "task": todo.get('title'),
+                "completed": todo.get('completed')
+                })
+    todoAll[user.get('id')] = taskList
+
+with open('todo_all_employees.json', mode='w') as f:
+    json.dump(todoAll, f)
