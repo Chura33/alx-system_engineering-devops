f8ad174239d460bcac46040bb4d14d6114e850e6
Chura33 <churabrady@gmail.com>
2023-11-08 05:16:49 -0800

    sovled task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,8 +8,13 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=''):
+    """ Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
+
+    - If not a valid subreddit, return None.
+    """
     res = requests.get(
-        "https://www.reddit.com/r//{}/hot.json".format(subreddit),
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={"User-Agent": "Custom"},
         params={"after": after},
     )
@@ -24,6 +29,6 @@ def recurse(subreddit, hot_list=[], after=''):
         if after is None:
             return hot_list
         else:
-            return recurse(subreddit, after)
+            return recurse(subreddit, hot_list, after)
     else:
         return None
