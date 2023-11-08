b65ff5b4722eb4b1a61b2eac4ea0e61b1dc69b65
Chura33 <churabrady@gmail.com>
2023-11-08 02:07:01 -0800

    solved task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-""" This module prints the titles of the top ten posts in a
-given subreddit"""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """ This function gets the title of the ten hottests posts"""
-
-    res = requests.get(
-            "https://www.reddit.com/r/{subreddit}/hot.json".format(subreddit),
-            headers={"User-Agent": "Custom"},
-            params={"limit": 10},
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
     )
 
-    if res.status_code == 200:
+    if req.status_code == 200:
         for get_data in req.json().get("data").get("children"):
-            obj = get_data.get("data")
-            title = get_data.get("title")
+            dat = get_data.get("data")
+            title = dat.get("title")
             print(title)
     else:
-        print(none)
+        print(None)
